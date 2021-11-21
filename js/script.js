40135358f602a6cd6dcf4a7d3741ecee6501509c
lnalmeida <l.n.almeida.ti@gmail.com>
2021-11-21 15:08:02 -0300

    adicionado validaçao basica dos inputs

--- js/script.js
+++ js/script.js
@@ -2,35 +2,37 @@ let result = document.querySelector('.result');
 let calculating = document.querySelector('.calculating');
 let splash = document.querySelector('.splash');
 let button = document.getElementById('button-calc');
-let pessoa1 = document.querySelector('.pessoa1').value;
-let pessoa2 = document.querySelector('.pessoa2').value;
+let pessoa1 = document.getElementById('pessoa1');
+let pessoa2 = document.getElementById('pessoa2');
 
 calculating.classList.toggle('hide');
 
 button.addEventListener('click', () => {
     console.log('click');
-    calculating.classList.toggle('hide');
-    // verifyInput();
-    splash.style.display = 'none';
-    let random = Math.floor(Math.random() * 100);
-    setTimeout(() => {    
-        result.innerHTML = `${random}%`;
-        calculating.style.display = 'none';
-        setTimeout(() => {
-            window.location.reload();
-        }, 3000)
-    }, 3000);
-    result.classList.toggle('hidde');
-    button.disabled = true;
-
+    if (verifyInput()) {
+        calculating.classList.toggle('hide');
+        splash.style.display = 'none';
+        let random = Math.floor(Math.random() * 100);
+        setTimeout(() => {    
+            result.innerHTML = `${random}%`;
+            calculating.style.display = 'none';
+            setTimeout(() => {
+                window.location.reload();
+            }, 5000)
+        }, 3000);
+        result.classList.toggle('hidde');
+        button.disabled = true;
+    } else {
+        alert('É preciso preencher ambos os campos para fazer o cálculo...');
+    }
 });
 
 function verifyInput() {
-    if (pessoa1 === '') {
-        alert('Введите имя первого участника');
-    } else if (pessoa2 === '') {
-        alert('Введите имя второго участника');
-    }
+    console.log(pessoa1.value, pessoa2.value);
+    if (pessoa1.value === '' || pessoa2.value === '') {
+        return false;
+    } 
+    return true;
 }
 
 
